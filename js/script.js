8a311614cf4a9562f89a8fb41d3ba9c1020e9945
camila-cavalcante23 <camilacavalcante7405@gmail.com>
2025-04-28 23:19:16 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
+const track = document.getElementById('carouselTrack');
+const dots = document.querySelectorAll('.dot');
+
+function moveToSlide(slideIndex) {
+  const slideWidth = track.clientWidth;
+  track.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
+
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[slideIndex].classList.add('active');
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const pontos = document.querySelector('.pontos');
     const menuOpcoes = document.querySelector('.menu-opcoes');
   
     pontos.addEventListener('click', () => {
-      // Alterna a visibilidade do menu de opções
+  
       if (menuOpcoes.style.display === 'none' || menuOpcoes.style.display === '') {
         menuOpcoes.style.display = 'block'; // Exibe o menu
       } else {
@@ -11,4 +23,50 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+
+ 
+
+  const lupa = document.querySelector('.lupa');
+  const campoBusca = document.querySelector('.barra-pesquisa input');
+
+
+  const resultadoBusca = document.createElement('div');
+  resultadoBusca.style.marginTop = '10px';
+  document.querySelector('.barra-pesquisa').appendChild(resultadoBusca);
+
+
+  lupa.addEventListener('click', () => {
+    const textoBusca = campoBusca.value.trim();
+    if (textoBusca !== '') {
+      resultadoBusca.textContent = `Você buscou por: '${textoBusca}'`;
+    } else {
+      resultadoBusca.textContent = '';
+    }
+  });
+
+
+  var swiper = new Swiper('.product-swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      480: {
+        slidesPerView: 1,
+      }
+    }
+  });
+
   
+ 
